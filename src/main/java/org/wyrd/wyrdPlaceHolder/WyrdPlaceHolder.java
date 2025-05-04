e2378f5d71cff62341faa36f4f2bd9180933b222
hsj0621 <sungju7259@gmail.com>
2025-05-04 12:44:16 +0900

    init

---
+++ src/main/java/org/wyrd/wyrdPlaceHolder/WyrdPlaceHolder.java
@@ -0,0 +1,19 @@
+package org.wyrd.wyrdPlaceHolder;
+
+import lombok.Getter;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WyrdPlaceHolder extends JavaPlugin {
+
+    @Getter
+    private static WyrdPlaceHolder inst;
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
